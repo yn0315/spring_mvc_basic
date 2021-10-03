0c788972311114e3afe32471c9a096a26687e182
yn0315 <yn0315@naver.com>
2021-10-03 16:10:10 +0900

    10.03 숙제완성

--- src/main/java/com/spring/mvc/score/repository/MemoryScoreRepository.java
+++ src/main/java/com/spring/mvc/score/repository/MemoryScoreRepository.java
@@ -43,7 +43,7 @@ public class MemoryScoreRepository implements  ScoreRepository{
     //점수 꺼내서 보여주는 처리
     @Override
     public Score findOne(int stuNum) {
-        log.info(scoreMap.get(stuNum));
+
         Score score = null;
         for (int i = 0; i < scoreMap.size(); i++) {
             score = scoreMap.get(stuNum);
